61e314a77d0178090af2a63de3a3018c1f010fc9
anaBeatrizWalker <ana04login@gmail.com>
2022-08-19 19:47:20 -0300

    Introdução

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
+let title: string = 'My First App';
+let paragraph: string =
+  'Parágrafo qualquer com quebra de linha na template string';
+
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>${title}</h1>
+                    <p>${paragraph}</p>`; //template string
+
+//Console.log
+console.log('Olá mundo!');
+
+//Laço
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+//Mistura de laço com condicional
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(`${i} é par`);
+  }
+}
