18cad31f16c9ffb3eb65f4d6893828531b1cc526
anaBeatrizWalker <ana04login@gmail.com>
2022-08-19 20:01:48 -0300

    Temporizador

--- index.ts
+++ index.ts
@@ -1,25 +1,18 @@
 // Import stylesheets
 import './style.css';
 
+//Temporizador
+
 let title: string = 'My First App';
-let paragraph: string =
-  'Parágrafo qualquer com quebra de linha na template string';
+document.getElementById('app.title').innerHTML = title;
 
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>${title}</h1>
-                    <p>${paragraph}</p>`; //template string
-
-//Console.log
-console.log('Olá mundo!');
-
-//Laço
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-}
-
-//Mistura de laço com condicional
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 == 0) {
-    console.log(`${i} é par`);
-  }
-}
+let counter: number = 0;
+/*
+setInterval(
+  nome da função que será executada, 
+  de quanto em quanto tempo será executada (m milisegundos))
+*/
+let intervalId = setInterval(() => {
+  counter++; //counter = counter + 1
+  document.getElementById('app.counter').innerHTML = counter.toString();
+}, 1000);
