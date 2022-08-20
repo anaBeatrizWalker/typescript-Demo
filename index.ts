4850894b8cf0da6ab23b268095ee255aa3858f9e
anaBeatrizWalker <ana04login@gmail.com>
2022-08-19 22:30:42 -0300

    Tarefa Temporizador Tabata

--- index.ts
+++ index.ts
@@ -1,18 +1,43 @@
 // Import stylesheets
 import './style.css';
 
-//Temporizador
-
 let title: string = 'My First App';
 document.getElementById('app.title').innerHTML = title;
 
-let counter: number = 0;
-/*
-setInterval(
-  nome da função que será executada, 
-  de quanto em quanto tempo será executada (m milisegundos))
-*/
-let intervalId = setInterval(() => {
-  counter++; //counter = counter + 1
-  document.getElementById('app.counter').innerHTML = counter.toString();
-}, 1000);
+//Tarefa: Temporizador Tabata
+let round: number = 1;
+let timer: number = 0;
+let rest: number = 0;
+
+let treino = setInterval(() => {
+  timer++;
+  if (timer <= 20) {
+    timer == 0;
+    document.getElementById('app.subtitleGo').innerHTML = `Go!`;
+    document.getElementById('app.go').innerHTML = timer.toString();
+
+    document.getElementById('app.subtitleRound').innerHTML = `Round:`;
+    document.getElementById('app.round').innerHTML = round.toString();
+  }
+
+  if (timer > 20) {
+    rest++;
+    if (rest <= 10) {
+      document.getElementById('app.subtitleRest').innerHTML = `Rest!`;
+      document.getElementById('app.rest').innerHTML = rest.toString();
+
+      document.getElementById('app.subtitleGo').style.display = 'none';
+      document.getElementById('app.go').style.display = 'none';
+    }
+  }
+  if (timer > 30 && round < 8) {
+    document.getElementById('app.subtitleGo').style.display = 'block';
+    document.getElementById('app.go').style.display = 'block';
+
+    document.getElementById('app.subtitleRound').innerHTML = `Round:`;
+    document.getElementById('app.round').innerHTML = round.toString();
+    round++;
+    timer = 0;
+    rest = 0;
+  }
+}, 100);
